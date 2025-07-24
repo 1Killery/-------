6c60f9ab2b4770a810b7c3f5c7c5bd59cefd1c1f
Dobrslav <ivlevdobroslav@gmail.com>
2025-07-24 20:54:00 +0300

    .

--- ну пробуем/code/player.gd
+++ ну пробуем/code/player.gd
@@ -1,17 +1,15 @@
 extends KinematicBody2D
 
+var velos = Vector2.ZERO
 var speed = 150
 
 func _process(delta):
-	var move = move()
-	var direction = move.normalized()
-	
-	
-
-func _ready():
-	pass
-
-func move():
-	var move_x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	var move_y = Input.get_action_strength("down") - Input.get_action_strength("up")
-	return Vector2(move_x,move_y)
+	if Input.is_action_just_pressed("right"):
+		velos.x = velos.normalized() * speed
+	elif Input.is_action_just_pressed("left"):
+		velos.x = velos.normalized() * speed
+		
+	else: 
+		velos.x = 0
+		
+	move_and_slide(velos)
