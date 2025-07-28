d6f6e96854edd27e37970d01712dbfd9520fd9cf
Dobrslav <ivlevdobroslav@gmail.com>
2025-07-28 20:14:17 +0300

    движение

--- ну пробуем/code/player.gd
+++ ну пробуем/code/player.gd
@@ -1,15 +1,70 @@
 extends KinematicBody2D
 
-var velos = Vector2.ZERO
-var speed = 150
+enum {
+	IDLE
+	RUN,
+	ATTACK,
+	HIT,
+	DEAD,
+	
+	
+}
 
-func _process(delta):
-	if Input.is_action_just_pressed("right"):
-		velos.x = velos.normalized() * speed
-	elif Input.is_action_just_pressed("left"):
-		velos.x = velos.normalized() * speed
+const speed = 150
+const state = "IDLE"
+
+onready var anim_sprait = $AnimatedSprite
+onready var animp = $AnimationPlayer
+
+var velocity = Vector2.ZERO
+var health = 100
+
+func _ready():
+	anim_sprait.play("idle")
+
+func _physics_process(delta):
+	if health <= 0 :
+		health = 0
+		#state = DEAD
+	
+	match state:
+		"RUN":
+			run()
+#		"ATTACK":
+#			attack()
+#		"HIT":
+#			hit_state()
+#		"DEAD":
+#			dead()
+#
+	
+	
+	
+	run()
+	update_anim()
+
+func update_anim() : 
+	if velocity:
+		anim_sprait.play("run")
+	else:
+		anim_sprait.play("idle")
 		
-	else: 
-		velos.x = 0
-		
-	move_and_slide(velos)
+	if velocity.x<0:
+		anim_sprait.flip_h = true
+		$CollisionShape2D.position.x = 2
+	elif velocity.x>0:
+		anim_sprait.flip_h = false
+		$CollisionShape2D.position.x = -2
+
+func run():
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("right"):
+		velocity.x += speed
+	if Input.is_action_pressed("left"):
+		velocity.x-= speed
+	if Input.is_action_pressed("down"):
+		velocity.y+= speed
+	if Input.is_action_pressed("up"):
+		velocity.y -= speed
+	velocity = velocity.normalized() * speed
+	move_and_slide(velocity)
---
+++ ну пробуем/player.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+var speed = 150
+var direction = Vector2.ZERO
+
+
+func _physics_process(delta): 
+	get_input()
+	
+	
+
+func get_input():
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	direction = velocity.normalized() * speed
