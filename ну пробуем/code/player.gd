9d3a3ff332070c5465b76f5763377f19ebb431ce
Dobrslav <ivlevdobroslav@gmail.com>
2025-08-01 21:08:49 +0300

    ..

--- ну пробуем/code/player.gd
+++ ну пробуем/code/player.gd
@@ -1,35 +1,30 @@
 extends KinematicBody2D
 
 enum {
-	IDLE,
 	RUN,
 	ATTACK,
-	HIT,
-	DEAD,
 	DOUBLE_ATTACK,
+	HIT,
 	BLOCK
 	
 	
 }
 
 const speed = 150
-const state = "IDLE"
+
 
 onready var anim_sprait = $AnimatedSprite
 onready var animp = $AnimationPlayer
 
 var velocity = Vector2.ZERO
 var health = 100
+var state = "RUN"
 
 func _ready():
 	anim_sprait.play("idle")
 
 # warning-ignore:unused_argument
 func _physics_process(delta):
-	if health <= 0 :
-		health = 0
-		#state = DEAD
-	
 	match state:
 		"RUN":
 			run()
@@ -37,21 +32,22 @@ func _physics_process(delta):
 #			attack()
 #		"HIT":
 #			hit_state()
-		"DEAD":
-			dead()
-#
+		"BLOCK":
+			block()
 	
 	
+	if health <= 0 :
+		health = 0
+		animp.play("dead")
+		yield(animp, "animation_finished")
 	
-	run()
 	update_anim()
-	dead()
 
 func update_anim() : 
 	if velocity:
-		anim_sprait.play("run")
+		animp.play("run")
 	else:
-		anim_sprait.play("idle")
+		animp.play("idle")
 		
 	if velocity.x<0:
 		anim_sprait.flip_h = true
@@ -59,7 +55,8 @@ func update_anim() :
 	elif velocity.x>0:
 		anim_sprait.flip_h = false
 		$CollisionShape2D.position.x = -2
-
+	move_and_slide(velocity)
+	
 func run():
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed("right"):
@@ -71,10 +68,13 @@ func run():
 	if Input.is_action_pressed("up"):
 		velocity.y -= speed
 	velocity = velocity.normalized() * speed
-# warning-ignore:return_value_discarded
-	move_and_slide(velocity)
 	
-func dead():
-	health <= 0 
-	health = 0
-	animp.play("dead")
+	if Input.is_action_pressed("block"):
+		state = BLOCK
+# warning-ignore:return_value_discarded
+
+func block():
+	animp.play("block")
+	yield(animp, "animation_finished")
+	state = RUN
+	
